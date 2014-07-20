c37b64744b8ea443742268d041a411b88f8df3d9
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-07-20 22:18:18 +0200

    First version of go-config

---
+++ config/config.go
@@ -0,0 +1,126 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/mitchellh/mapstructure"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+type Config map[string]interface{}
+type Configs map[string]interface{}
+
+var basePath string
+var env string
+var configs Configs
+
+// Init initalize the config system with the given base path.
+func Init(dir string, environment string) error {
+	var err error
+	basePath = dir
+	configFiles, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		return err
+	}
+
+	configs = make(Configs, len(configFiles))
+	env = os.Getenv(environment)
+
+	return nil
+}
+
+// Get returns the config for the given domain.
+func Get(domain string, rawVal interface{}) (err error) {
+	if _, ok := configs[domain]; ok {
+		rawVal = configs[domain]
+		return nil
+	}
+
+	var baseFile io.Reader
+	var domainFile io.Reader
+	baseFile, err = load(domain + ".json")
+	if err != nil {
+		return
+	}
+
+	var baseConfig Config
+	var domainConfig Config
+	baseConfig, err = marshalReader(baseFile)
+	if err != nil {
+		return
+	}
+
+	if env != "" {
+		domainConfFound := true
+		domainFile, err = load(domain + "." + env + ".json")
+		if err != nil {
+			domainConfFound = false
+		}
+
+		if domainConfFound {
+			domainConfig, err = marshalReader(domainFile)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	err = marshal(baseConfig, domainConfig, rawVal)
+	if err != nil {
+		return
+	}
+
+	if _, ok := configs[domain]; !ok {
+		configs[domain] = rawVal
+	}
+
+	return
+}
+
+// load loads the config file with the given file name from disk.
+func load(filename string) (r io.Reader, err error) {
+	if _, err = os.Stat(path.Join(basePath, filename)); err != nil {
+		return nil, err
+	}
+
+	file, err := ioutil.ReadFile(path.Join(basePath, filename))
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewReader(file), nil
+}
+
+func marshalReader(r io.Reader) (confMap Config, err error) {
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(r)
+	if err != nil {
+		return nil, err
+	}
+
+	confMap = make(Config)
+	err = json.Unmarshal(buf.Bytes(), &confMap)
+	if err != nil {
+		return nil, err
+	}
+	return confMap, nil
+}
+
+func marshal(a Config, b Config, rawVal interface{}) (err error) {
+	err = mapstructure.Decode(&a, rawVal)
+	if err != nil {
+		return
+	}
+
+	if b != nil {
+		err = mapstructure.Decode(&b, rawVal)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
---
+++ config/config_test.go
@@ -0,0 +1,153 @@
+package config
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"os"
+	"testing"
+)
+
+type BasicConfig struct {
+	Host    string
+	Port    int
+	ApiKey  string
+	Twitter struct {
+		Username string
+		Password string
+	}
+	Facebook struct {
+		Username string
+		Password string
+	}
+}
+
+type RedisConfig struct {
+	Host         string
+	Port         int
+	Username     string
+	Password     string
+	OptionInBase string
+}
+
+func TestInitialization(t *testing.T) {
+	Convey("Initializing the library", t, func() {
+		os.Setenv("GOENV", "stage")
+		Init("../data", "GOENV")
+
+		Convey("When setting GOENV", func() {
+			Convey("to stage", func() {
+				Convey("env should be stage", func() {
+					So(env, ShouldEqual, "stage")
+				})
+			})
+		})
+	})
+}
+
+func TestGetDomainBaseConfig(t *testing.T) {
+	var basicConfig BasicConfig
+	err := Get("basic", &basicConfig)
+	Convey("When reading options from base.json", t, func() {
+		Convey("No error should happen", func() {
+			So(err, ShouldEqual, nil)
+		})
+
+		Convey("When getting first levele options", func() {
+			Convey("The properties should have the right values", func() {
+				So(basicConfig.Host, ShouldEqual, "localhost")
+				So(basicConfig.Port, ShouldEqual, 3030)
+				So(basicConfig.ApiKey, ShouldEqual, "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")
+			})
+		})
+
+		Convey("When reading second level options", func() {
+			Convey("The properties should have the right values", func() {
+				So(basicConfig.Twitter.Username, ShouldEqual, "mytwittername")
+				So(basicConfig.Twitter.Password, ShouldEqual, "mytwitterpassword")
+				So(basicConfig.Facebook.Username, ShouldEqual, "myfacebookname")
+				So(basicConfig.Facebook.Password, ShouldEqual, "myfacebookpassword")
+			})
+		})
+	})
+}
+
+func TestGetDomainRedisConfig(t *testing.T) {
+	Convey("Initializing with empty environment", t, func() {
+		os.Setenv("GOENV", "")
+		Init("../data", "GOENV")
+		var redisConf RedisConfig
+		err := Get("redis", &redisConf)
+
+		Convey("When reading options from redis.json", func() {
+			Convey("No errors should happen", func() {
+				So(err, ShouldEqual, nil)
+			})
+
+			Convey("The right properties should be loaded", func() {
+				So(redisConf.Host, ShouldEqual, "redis.host.com")
+				So(redisConf.Port, ShouldEqual, 6379)
+				So(redisConf.Username, ShouldEqual, "test")
+				So(redisConf.Password, ShouldEqual, "password")
+			})
+		})
+	})
+
+	Convey("Initializing with environment set to ci", t, func() {
+		os.Setenv("GOENV", "ci")
+		Init("../data", "GOENV")
+		var redisConf RedisConfig
+		err := Get("redis", &redisConf)
+
+		Convey("When reading options from redis.ci.json", func() {
+			Convey("No errors should happen", func() {
+				So(err, ShouldEqual, nil)
+			})
+
+			Convey("The right properties should be loaded", func() {
+				So(redisConf.Host, ShouldEqual, "redis.ci.host.com")
+				So(redisConf.Port, ShouldEqual, 6379)
+				So(redisConf.Username, ShouldEqual, "ciuser")
+				So(redisConf.Password, ShouldEqual, "cipassword")
+			})
+		})
+	})
+
+	Convey("Initializing with environment set to stage", t, func() {
+		os.Setenv("GOENV", "stage")
+		Init("../data", "GOENV")
+		var redisConf RedisConfig
+		err := Get("redis", &redisConf)
+
+		Convey("When reading options from redis.stage.json", func() {
+			Convey("No errors should happen", func() {
+				So(err, ShouldEqual, nil)
+			})
+
+			Convey("The right properties should be loaded", func() {
+				So(redisConf.Host, ShouldEqual, "redis.stage.host.com")
+				So(redisConf.Port, ShouldEqual, 8080)
+				So(redisConf.Username, ShouldEqual, "stageuser")
+				So(redisConf.Password, ShouldEqual, "stagepassword")
+			})
+		})
+	})
+
+	Convey("Initializing with environment set to prod", t, func() {
+		os.Setenv("GOENV", "prod")
+		Init("../data", "GOENV")
+		var redisConf RedisConfig
+		err := Get("redis", &redisConf)
+
+		Convey("When reading options from redis.prod.json", func() {
+			Convey("No errors should happen", func() {
+				So(err, ShouldEqual, nil)
+			})
+
+			Convey("The right properties should be loaded", func() {
+				So(redisConf.Host, ShouldEqual, "redis.prod.host.com")
+				So(redisConf.Port, ShouldEqual, 9990)
+				So(redisConf.Username, ShouldEqual, "produser")
+				So(redisConf.Password, ShouldEqual, "prodpassword")
+			})
+		})
+	})
+}
