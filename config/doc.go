b806fcbed61a07facb9467f7d183bfabcc06b8e9
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-07-22 23:36:42 +0200

    updated go-doc

--- config/doc.go
+++ config/doc.go
@@ -8,6 +8,33 @@ you to use cascading configuration files (e.g. one configuration for every envir
 You can define options for every environment in different configuration files and
 load them accordingly to the set environment.
 
+Usage
+
+Place your configuration files in a directory (e.g. config). Configuration files
+need to be valid JSON files, terminated by .json.
+
+The following example loads and parses options from base.json in the config directory:
+
+    package main
+
+    import (
+        "fmt"
+        "github.com/hypebeast/go-config/config"
+    )
+
+    type BaseConfig struct {
+        Host    string
+        Port    int
+    }
+
+    func main() {
+        // Initialize the config system
+        config.Init("config", "")
+
+        // Get the base config
+        config.Get("base", &baseConfig)
+    }
+
 */
 
 package config
